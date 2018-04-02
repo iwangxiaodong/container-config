b11317b06d7bff33136b7b95e9c0eaa69fa9d160
iwangxiaodong <i@wangxiaodong.com>
2018-04-02 16:49:29 +0800

    Update myext.go

--- fn/myext/myext.go
+++ fn/myext/myext.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/fnproject/fn/api/server"
 	"github.com/fnproject/fn/fnext"
 )
@@ -25,7 +26,7 @@ func (e *myExt) Name() string {
 func (e *myExt) Setup(s fnext.ExtServer) error {
 	s.AddAPIMiddlewareFunc(func(next http.Handler) http.Handler {
 		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			fmt.Println("My ext - ", time.Now())
+			fmt.Println("My ext AddAPIMiddlewareFunc - ", time.Now())
 
 			ctx := r.Context()
 			authorizationHeader := r.Header.Get("Authorization")
@@ -40,12 +41,61 @@ func (e *myExt) Setup(s fnext.ExtServer) error {
 				return
 			}
 
-			if ahSplit[1] != "token13572468" {
-				server.WriteError(ctx, w, http.StatusUnauthorized, errors.New("Invalid authorization token, access denied"))
+			// if ahSplit[1] != "token13572468" {
+			// 	server.WriteError(ctx, w, http.StatusUnauthorized, errors.New("Invalid authorization token, access denied"))
+			// 	return
+			// }
+
+			idToken := ahSplit[1]
+			fmt.Println("idToken - " + idToken)
+
+			// 后续从数据库获取用户TokenSecret
+			var jwtSecret = "92d75b10-358d-11e8-a6fc-0a580a340088"
+
+			jwtToken, err := jwt.Parse(idToken, func(jt *jwt.Token) (interface{}, error) {
+				if _, ok := jt.Method.(*jwt.SigningMethodHMAC); !ok {
+					return nil, fmt.Errorf("Unexpected signing method: %v", jt.Header["alg"])
+				}
+				return []byte(jwtSecret), nil
+			})
+			if err != nil {
+				w.WriteHeader(http.StatusUnauthorized)
+				fmt.Fprintf(w, err.Error())
 				return
 			}
 
+			if !jwtToken.Valid {
+				w.WriteHeader(http.StatusUnauthorized)
+				fmt.Fprintf(w, errors.New("Invalid authorization token, access denied").Error())
+				return
+			}
+			var claims jwt.MapClaims
+			var ok bool
+			if claims, ok = jwtToken.Claims.(jwt.MapClaims); !ok {
+				w.WriteHeader(http.StatusUnauthorized)
+				fmt.Fprintf(w, errors.New("Invalid authorization token, invalid claims, access denied").Error())
+				return
+			}
+			// ok, so finally we're good
+			username := claims["username"].(string)
+			fmt.Println("claims username - " + username)
+
+			appNameV := ctx.Value(fnext.AppNameKey)
+			if appNameV != nil {
+				appName := appNameV.(string)
+				fmt.Println("appName - " + appName)
+			}
+
 			fmt.Println("My ext ServeHTTP - ", time.Now())
+
+			next.ServeHTTP(w, r)
+		})
+	})
+
+	s.AddRootMiddlewareFunc(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+
+			fmt.Println("My ext AddRootMiddlewareFunc - ", time.Now())
 
 			next.ServeHTTP(w, r)
 		})
